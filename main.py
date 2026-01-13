8e77d8d6729f9b956b74f8b1e6aa1910e458357a
korendn-sys <korendn@gmail.com>
2026-01-13 22:21:09 +0200

    Implement edge detection and save output image
    Added edge detection and visualization functionality.

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import matplotlib.pyplot as plt
 
+
+clean_image = median(img_array, ball(3))
+
+edge_mag_image = edge_detection(clean_image)
+
+plt.hist(edge_mag_image.ravel(), bins=50)
+plt.title('Histogram of Edge Magnitudes')
+plt.xlabel('Magnitude')
+plt.ylabel('Frequency')
+plt.show()
+
+threshold = 50 
+edge_binary = edge_mag_image > threshold
+
+edge_binary_display = (edge_binary * 255).astype(np.uint8)
+
+edge_image = Image.fromarray(edge_binary_display)
+
+plt.imshow(edge_image, cmap='gray')
+plt.title('Binary Edge-Detected Image')
+plt.axis('off')
+plt.show()
+
+edge_image.save('my_edges.png')
+print("Edge-detected image saved as 'my_edges.png'")
