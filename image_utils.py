38a42da3b61a2e26314a54697e8dab2235a344d9
korendn-sys <korendn@gmail.com>
2026-01-13 22:18:13 +0200

    Implement image loading and edge detection functions

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,27 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    img = Image.open(path).convert('RGB')
+    return np.array(img)
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+    grayscale_image = np.mean(image_array, axis=2)
+
+    kernelY = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ])
+
+    kernelX = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ])
+
+    edgeY = convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeX = convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+
+    return edgeMAG
